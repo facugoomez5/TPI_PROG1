c96497f5acb4dbe4b5510ad1dddb039148a254a3
Facundo Gomez <facu1131@gmail.com>
2026-06-11 07:36:14 -0300

    agregando funciones de validacion

--- main.py
+++ main.py
@@ -28,5 +28,39 @@ def mostrar_paises(paises):
             f"Continente: {pais['continente']}"
         )
 
-paises = cargar_paises("paises.csv")
-mostrar_paises(paises)
+def buscar_pais(paises, nombre):
+    for pais in paises:
+        if pais["nombre"].strip().lower() == nombre.strip().lower():
+            return pais
+    return None
+
+def validar_pais(nombre):
+    while True:
+        try:    
+            if nombre == "":
+                print()
+                raise ValueError("El nombre no puede estar vacío.")
+            
+            if not nombre.replace(" ", "").isalpha():
+                print()
+                raise ValueError("El nombre debe contener solo letras.")
+            
+            return nombre.strip()
+        
+        except ValueError as e:
+            print("Error:", e)     
+
+def validar_entero(mensaje, minimo=1):
+    while True:
+        try:
+            numero = int(input(mensaje))
+            if numero < minimo:
+                print(f"Error: El número debe ser mayor o igual a {minimo}.")
+            
+            return numero
+        except ValueError as e:
+            if "invalid literal" in str(e):
+                print("Error: Por favor, ingrese un número válido.")
+            else:
+                print(f"Error: {e}")
+
